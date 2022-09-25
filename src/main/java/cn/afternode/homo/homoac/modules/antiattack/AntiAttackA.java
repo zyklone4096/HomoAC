6acd9fa7930a1204af1008109f1ae82d084b1a2e
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-09-25 18:23:06 +0800

    更换事件

--- src/main/java/cn/afternode/homo/homoac/modules/antiattack/AntiAttackA.java
+++ src/main/java/cn/afternode/homo/homoac/modules/antiattack/AntiAttackA.java
@@ -1,11 +1,13 @@
 package cn.afternode.homo.homoac.modules.antiattack;
 
+import cn.afternode.homo.homoac.ExtProperties;
 import cn.afternode.homo.homoac.HomoAC;
 import cn.afternode.homo.homoac.utils.module.ChildrenModule;
 import cn.afternode.homo.homoac.utils.module.Module;
 import cn.afternode.homo.homoac.utils.module.StdEventListener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 import java.util.regex.Pattern;
 
@@ -20,10 +22,10 @@ public class AntiAttackA extends ChildrenModule implements StdEventListener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerLoginEvent event) {
         if (HomoAC.PROP_ENABLED && HomoAC.EXT_PROPERTY.EMP_ENABLED && isEMPBot(event.getPlayer().getName())) {
             HomoAC.LOGGER.warning("匹配EMPv2随机名称逻辑，已踢出玩家：" + event.getPlayer().getName() + " (" + event.getPlayer().getUniqueId().toString() +")");
-            event.getPlayer().kickPlayer(HomoAC.EXT_PROPERTY.EMP_KICK_MSG);
+            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, HomoAC.EXT_PROPERTY.EMP_KICK_MSG);
         }
     }
 }
