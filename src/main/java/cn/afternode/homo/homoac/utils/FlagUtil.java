cbd09bec4e362b53991386b41f77ece0b9aa5b72
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-09-25 18:23:27 +0800

    新回弹

--- src/main/java/cn/afternode/homo/homoac/utils/FlagUtil.java
+++ src/main/java/cn/afternode/homo/homoac/utils/FlagUtil.java
@@ -2,10 +2,21 @@ package cn.afternode.homo.homoac.utils;
 
 import cn.afternode.homo.homoac.utils.user.User;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class FlagUtil {
     public static void flagWithEvent(User user, PacketReceiveEvent event) {
         user.PLAYER.teleport(user.lastULocation);
+        event.setCancelled(true);
+        user.updateLocation();
+    }
+
+    public static void flagNoEvent(User user) {
+        user.PLAYER.teleport(user.lastULocation);
+        user.updateLocation();
+    }
+
+    public static void flagBukkitEvent(User user, PlayerMoveEvent event) {
         event.setCancelled(true);
         user.updateLocation();
     }
